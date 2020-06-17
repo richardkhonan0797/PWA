afb569f14ea16886de3b50ce1b99bc2208ba0a4b
Richard Khonan <richardkhonan0797@gmail.com>
2020-06-18 00:48:41 +0700

    first commit

---
+++ client/public/serviceworker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'version-1'
+const urlsToCache = [
+    'index.html',
+    'offline.html'
+]
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then( cache => {
+                console.log('Opened cache')
+                cache.addAll(urlsToCache)
+            })
+    )
+})
+
+// Listern request
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                          .catch(() => caches.match('offline.html'))
+            })
+    )
+})
+
+// activate sw  
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = []
+    cacheWhiteList.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys()
+            .then( cachesNames => {
+                Promise.all(
+                    cachesNames.map( cacheName => {
+                        if (!cacheWhiteList.includes(cacheName)) {
+                            return caches.delete(cacheName)
+                        }
+                    })
+                )
+            })
+    )
+})
